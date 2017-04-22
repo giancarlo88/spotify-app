99679a5d706762bfb79086bb140ec44f71950b88
Giancarlo Galliani <galliani.08@gmail.com>
2017-04-22 16:10:17 +0100

    Initial commit.

---
+++ test/utils/config.js
@@ -0,0 +1,33 @@
+require('babel-core/register')({
+    ignore: /node_modules/,
+    plugins: ['react-require'],
+    extensions: ['.js']
+});
+
+// var jsdom = require('jsdom')
+var jsdom = require('jsdom').jsdom
+
+// setup the simplest document possible
+var doc = jsdom('<!doctype html><html><body></body></html>')
+
+// get the window object out of the document
+var win = doc.defaultView
+
+// set globals for mocha that make access to document and window feel
+// natural in the test environment
+global.document = doc
+global.window = win
+
+// take all properties of the window object and also attach it to the
+// mocha global object
+propagateToGlobal(win)
+
+// from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
+function propagateToGlobal (window) {
+  for (let key in window) {
+    if (!window.hasOwnProperty(key)) continue
+    if (key in global) continue
+
+    global[key] = window[key]
+  }
+}
